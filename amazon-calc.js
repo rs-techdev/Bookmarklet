2f9e43c25b4f5c2ed554edc7e14e48d8d73bbfba
RikusoraProject <rikusoraproject@gmail.com>
2022-05-21 19:35:27 +0900

    詳細データ取得

--- amazon-calc.js
+++ amazon-calc.js
@@ -1,100 +1,169 @@
-javascript: (
-  function () {
+javascript: (function () {
     let total = 0;
-    let content = '';
     let pageNum = 0;
+    let output = {};
+    let output2 = [];
 
     function parseHistory(text) {
-      const doc = new DOMParser().parseFromString(text, "text/html");
+        const doc = new DOMParser().parseFromString(text, "text/html");
 
-      // 注文点数から総ページ数を求める
-      const itemNumStr = doc.getElementsByClassName("num-orders")[0].textContent.replace(/[件,]/, '');
-      pageNum = Math.ceil(Number(itemNumStr.replace(/,/, '')) / 10);
+        // 注文点数から総ページ数を求める
+        const itemNumStr = doc.getElementsByClassName("num-orders")[0].textContent.replace(/[件,]/, "");
+        pageNum = Math.ceil(Number(itemNumStr.replace(/,/, "")) / 10);
 
-      // 注文毎に処理
-      const orders = doc.getElementsByClassName("a-box-group a-spacing-base order");
-      [...orders].forEach(order => {
-        const orderDate = order.getElementsByClassName("a-color-secondary value")[0].textContent.trim();
-        const orderId = order.getElementsByClassName("a-color-secondary value")[2].textContent.trim();
-        const orderPriceStr = order.getElementsByClassName("a-color-secondary value")[1].textContent.replace(/[￥ ,]/g, '')
-        const orderPrice = (Number(orderPriceStr) | 0);
-        total += orderPrice;
+        // 注文毎に処理
+        const orders = doc.getElementsByClassName("a-box-group a-spacing-base order");
+        [...orders].forEach((order) => {
+            const orderDate = order.getElementsByClassName("a-color-secondary value")[0].textContent.trim();
+            const orderId = order.getElementsByClassName("a-color-secondary value")[2].textContent.trim();
+            const orderPriceStr = order
+                .getElementsByClassName("a-color-secondary value")[1]
+                .textContent.replace(/[￥ ,]/g, "");
+            const orderPrice = Number(orderPriceStr) | 0;
+            total += orderPrice;
 
-        const itemEles = order.getElementsByClassName("a-fixed-left-grid-col a-col-right");
-        [...itemEles].forEach((item, index) => {
-          const itemName = item.getElementsByClassName("a-link-normal")[0].textContent.replace(/\t/g, " ").trim();
-          const itemUrl = item.getElementsByClassName("a-link-normal")[0].getAttribute("href");
-          // const shopName = item.getElementsByClassName("a-size-small a-color-secondary")[0]?.textContent?.replace(/ +販売: +|^\n/g, "")?.trim() ?? 'ショップ名無し';
-          // const priceStr = item.getElementsByClassName("a-size-small a-color-price")[0]?.textContent?.replace(/^\n/g, "")?.trim() ?? '\\0';
-          // content += `${orderId}\t${orderDate}\t${shopName}\t${itemName}\t${priceStr}\n`;
-          if (index == 0) {
-            content += `${orderId}\t${orderDate}\t${orderPrice}\t${itemName}\thttps://www.amazon.co.jp${itemUrl}\n`;
-          } else {
-            content += `${orderId}\t${orderDate}\t \t${itemName}\thttps://www.amazon.co.jp${itemUrl}\n`;
-          }
+            // 「注文日」と「ご請求額」は先に取得
+            output[orderId] = {
+                orderDate: orderDate,
+                orderPrice: orderPrice,
+            };
         });
-      });
+    }
+
+    function parseDetail(text) {
+        const doc = new DOMParser().parseFromString(text, "text/html");
+        try {
+            const detail = {};
+            const orderId = doc.getElementsByTagName("bdi")[0].textContent.trim();
+
+            const itemEles = doc.getElementById("od-subtotals")
+                ? doc.getElementById("od-subtotals").querySelectorAll("[class=a-row]")
+                : doc.getElementsByClassName("a-fixed-right-grid-col a-col-right")[0].querySelectorAll("[class=a-row]");
+
+            [...itemEles].forEach((item) => {
+                const key = item.getElementsByTagName("span")[0].textContent.trim();
+                const val = item
+                    .getElementsByTagName("span")[1]
+                    .textContent.trim()
+                    .replace(/[￥ ,]/g, "");
+                detail[key] = val;
+            });
+            output[orderId] = { ...output[orderId], ...detail };
+        } catch {}
+
+        try {
+            const orderId = doc.getElementsByTagName("bdi")[0].textContent.trim();
+            const orderDate = doc
+                .getElementsByClassName("order-date-invoice-item")[0]
+                .textContent.trim()
+                .replace("注文日 ", "");
+
+            const itemEles = doc.getElementsByClassName("yohtmlc-item");
+            [...itemEles].forEach((item) => {
+                const name = item.getElementsByClassName("a-link-normal")[0].textContent.trim();
+                const price = item
+                    .getElementsByClassName("a-color-price")[0]
+                    .textContent.trim()
+                    .replace(/[￥ ,]/g, "");
+                const num = item.parentElement.getElementsByClassName("item-view-qty")[0]?.textContent.trim() ?? 1;
+
+                output2.push({ orderId, orderDate, name, price, num });
+            });
+        } catch {}
     }
 
     async function calcPrice(year) {
-      const reqUrl = "https://www.amazon.co.jp/gp/css/order-history?disableCsd=no-js&orderFilter=year-" + year;
-      const text = await (await fetch(reqUrl)).text();
-      parseHistory(text);
+        const reqUrl = "https://www.amazon.co.jp/gp/css/order-history?disableCsd=no-js&orderFilter=year-" + year;
+        const text = await (await fetch(reqUrl)).text();
+        parseHistory(text);
 
-      if (pageNum > 1) {
-        const reqUrls = [...Array(pageNum - 1).keys()].map(i => "https://www.amazon.co.jp/gp/css/order-history?disableCsd=no-js&orderFilter=year-" + year + "&startIndex=" + ((i + 1) * 10));
-        await Promise.all(reqUrls.map(u => fetch(u))).then(responses =>
-          Promise.all(responses.map(res => res.text()))
-        ).then(texts => {
-          texts.forEach(text => parseHistory(text));
-        })
-      }
+        if (pageNum > 1) {
+            const reqUrls = [...Array(pageNum - 1).keys()].map(
+                (i) =>
+                    "https://www.amazon.co.jp/gp/css/order-history?disableCsd=no-js&orderFilter=year-" +
+                    year +
+                    "&startIndex=" +
+                    (i + 1) * 10
+            );
+            await Promise.all(reqUrls.map((u) => fetch(u)))
+                .then((responses) => Promise.all(responses.map((res) => res.text())))
+                .then((texts) => {
+                    texts.forEach((text) => parseHistory(text));
+                });
+        }
+
+        // 注文詳細を取得する
+        const reqUrls = Object.keys(output).map(
+            (orderId) => `https://www.amazon.co.jp/gp/your-account/order-details?orderID=${orderId}`
+        );
+        await Promise.all(reqUrls.map((u) => fetch(u)))
+            .then((responses) => Promise.all(responses.map((res) => res.text())))
+            .then((texts) => {
+                // texts.forEach((text) => parseOrder(text));
+                texts.forEach((text) => parseDetail(text));
+            });
+        // console.log(output);
     }
 
     function outputTsv() {
-      let win = window.open('', 'name', 'height=250,width=700');
-      win.document.write('<html><head><title>Amazon to TSV</title>');
-      win.document.write(`
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NBHQLZ3');</script>
-<!-- End Google Tag Manager -->
-</head>
-<body>
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NBHQLZ3" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
-`);
-      win.document.write('<pre>');
-      win.document.write('注文番号\t注文日\t金額\t商品名\tURL\n');
-      win.document.write(content);
-      win.document.write('</pre>');
-      win.document.write('</body></html>');
-      win.document.close();
-    }
+        let win = window.open("", "name", "height=250,width=700");
+        win.document.write("<html><head><title>Amazon to TSV</title>");
+        win.document.write(`
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-NBHQLZ3');</script>
+    <!-- End Google Tag Manager -->
+    </head>
+    <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NBHQLZ3" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    `);
+        win.document.write("<pre>");
+        win.document.write(
+            "注文番号\t注文日\t商品の小計\t配送料・手数料\t注文合計\tAmazonギフト券・Amazonショッピングカードの金額\tAmazonポイント\t割引\tご請求額\turl\t備考\n"
+        );
+        Object.keys(output).forEach((orderId) => {
+            const shoukei = output[orderId]["商品の小計："] ?? 0;
+            const haisou = output[orderId]["配送料・手数料："] ?? 0;
+            const goukei = output[orderId]["注文合計："] ?? 0;
+            const gift = output[orderId]["Amazonギフト券・Amazonショッピングカードの金額："] ?? 0;
+            const point = output[orderId]["Amazonポイント:"] ?? 0;
+            const seikyu = output[orderId]["ご請求額："] ?? 0;
+            const waribiki = output[orderId]["割引："] ?? 0;
+            const orderDate = output[orderId]["orderDate"] ?? "";
+            const url = `https://www.amazon.co.jp/gp/your-account/order-details?orderID=${orderId}`;
+            const remarks = output[orderId]["remarks"] ?? "";
 
-    const year = window.prompt("西暦何年のAmazonでの購入金額合計を調べますか？\n - 半角数字4桁で入力(2000年以降)\n - 全期間を調べる場合は「all」と入力");
-
-    if (!new RegExp(/^20[0-2][0-9]$|^all$/).test(year)) {
-      alert("2000年以降の西暦を正しく入力してください。");
-      return;
-    }
-
-    if (year == 'all') {
-      const yearStrList = [...document.querySelectorAll("#orderFilter option")]
-        .filter(option => option.value.match(/^year/))
-        .map(option => option.value.replace('year-', ''));
-
-      Promise.all(yearStrList.map(y => calcPrice(y))).then(() => {
-        alert(`全期間のAmazonでの使用金額合計は、${total.toLocaleString()}円です。`);
-        outputTsv();
-      });
-    } else {
-      calcPrice(year)
-        .then(() => {
-          alert(`${year}年のAmazonでの使用金額合計は、${total.toLocaleString()}円です。`);
-          outputTsv();
+            win.document.write(
+                `${orderId}\t${orderDate}\t${shoukei}\t${haisou}\t${goukei}\t${gift}\t${point}\t${waribiki}\t${seikyu}\t${url}\t${remarks}\n`
+            );
         });
+        win.document.write("\n");
+        win.document.write("注文番号\t注文日\t商品名\t購入数\t購入単価\t購入額\n");
+        output2.forEach((item) => {
+            const totalPrice = item.num * item.price;
+            win.document.write(
+                `${item.orderId}\t${item.orderDate}\t${item.name}\t${item.num}\t${item.price}\t${totalPrice}\n`
+            );
+        });
+        win.document.write("\n");
+        win.document.write("</pre>");
+        win.document.write("</body></html>");
+        win.document.close();
     }
-  }
-)();
+
+    const year = window.prompt(
+        "西暦何年のAmazonでの購入金額合計を調べますか？\n - 半角数字4桁で入力(2000年以降)を入力してください"
+    );
+
+    if (!new RegExp(/^20[0-2][0-9]$/).test(year)) {
+        alert("2000年以降の西暦を正しく入力してください。");
+        return;
+    }
+
+    calcPrice(year).then(() => {
+        alert(`${year}年のAmazonでの年間購入金額は、${total.toLocaleString()}円です。`);
+        outputTsv();
+    });
+})();
